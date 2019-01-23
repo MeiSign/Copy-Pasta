960c5346fb37a200853615b736018d8556190251
Stefan Meiwald <stefanmeiwald@yahoo.de>
2019-01-23 20:13:12 +0100

    Initial Spring boot app with simple controller

--- src/main/kotlin/de/meisign/copypasta/CopyPastaController.kt
+++ src/main/kotlin/de/meisign/copypasta/CopyPastaController.kt
@@ -3,6 +3,7 @@ package de.meisign.copypasta
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import java.util.*
 
 @Controller
 class CopyPastaController {
@@ -13,9 +14,14 @@ class CopyPastaController {
         return "uploadForm"
     }
 
-//    @GetMapping("/download")
-//    fun download() =
+    @GetMapping("/download")
+    fun download(): String {
 
-//    @GetMapping("/qrcode")
-//    fun qrcode() =
+    }
+
+    @GetMapping("/qrcode")
+    fun qrcode(model: Model): String {
+        model.addAttribute("id", UUID.randomUUID().toString())
+        return "qrCode"
+    }
 }
