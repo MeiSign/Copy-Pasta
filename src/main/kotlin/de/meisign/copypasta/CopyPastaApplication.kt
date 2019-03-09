45fd517a0e9c390b94c99f473c60354265a85942
Stefan Meiwald <stefanmeiwald@yahoo.de>
2019-03-09 15:26:41 +0100

    Use Java Configuration for spring cloud aws

--- src/main/kotlin/de/meisign/copypasta/CopyPastaApplication.kt
+++ src/main/kotlin/de/meisign/copypasta/CopyPastaApplication.kt
@@ -1,12 +1,16 @@
 package de.meisign.copypasta
 
+import de.meisign.copypasta.storage.s3.CloudConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Import
 
 
 @SpringBootApplication
+@Import(CloudConfiguration::class)
 class CopyPastaApplication
 
 fun main(args: Array<String>) {
     runApplication<CopyPastaApplication>(*args)
 }
+
--- src/main/kotlin/de/meisign/copypasta/storage/s3/CloudConfiguration.kt
+++ src/main/kotlin/de/meisign/copypasta/storage/s3/CloudConfiguration.kt
@@ -1,10 +1,36 @@
 package de.meisign.copypasta.storage.s3
 
+import com.amazonaws.client.builder.AwsClientBuilder
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.cloud.aws.context.config.annotation.EnableContextInstanceData
+import org.springframework.cloud.aws.context.config.annotation.EnableContextResourceLoader
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.ImportResource
+import org.springframework.context.annotation.Primary
 import org.springframework.context.annotation.Profile
 
 @Profile("!local")
 @Configuration
-@ImportResource("classpath*:cloudContext.xml")
-class CloudConfiguration
+@EnableContextInstanceData
+@EnableContextResourceLoader
+class CloudConfiguration {
+  @Value("\${s3.endpoint:https://s3.eu-west-1.amazonaws.com}")
+  val s3Endpoint: String = ""
+
+  @Value("\${s3.region:eu-west-1}")
+  val s3Region: String = ""
+
+  @Bean
+  @Primary
+  fun amazonS3Client(): AmazonS3 {
+    val endpointConfig = AwsClientBuilder.EndpointConfiguration(
+        s3Endpoint, s3Region)
+
+    return AmazonS3ClientBuilder
+        .standard()
+        .withEndpointConfiguration(endpointConfig)
+        .build()
+  }
+}
