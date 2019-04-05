e4dfcb93ef21869cd9dacb617a99de241f83231a
Stefan Meiwald <stefanmeiwald@yahoo.de>
2019-04-05 22:29:50 +0200

    Updating dependencies

--- src/main/kotlin/de/meisign/copypasta/storage/s3/S3Storage.kt
+++ src/main/kotlin/de/meisign/copypasta/storage/s3/S3Storage.kt
@@ -9,7 +9,6 @@ import kotlinx.coroutines.future.await
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Profile
 import org.springframework.core.io.ByteArrayResource
 import org.springframework.core.io.Resource
 import org.springframework.stereotype.Component
